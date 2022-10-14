de1b873dfc5329701ab8a1dec51a08c5073603a3
Hecey <hecey@hotmail.com>
2022-10-14 01:25:26 -0500

    Added pagination elements according to backend data

--- js/app.js
+++ js/app.js
@@ -1,16 +1,20 @@
-import { getTextAfterHash } from "../js/helper.js";
+import { toCapital, getTextAfterHash } from "../js/helper.js";
 import { buildHTMLItem } from "../js/item.js";
 import { buildHTMLCategory } from "../js/category.js";
+import { buildHTMLPagination } from "../js/pagination.js";
 
 const URL_PROXY = 'https://young-meadow-11122.herokuapp.com/'
 const URL_CATEGORIES = 'https://frozen-dawn-70616.herokuapp.com/categories'
 const URL_PRODUCTS = 'https://frozen-dawn-70616.herokuapp.com/products'
+const URL_PRODUCTS_GETNUMPAGES = 'https://frozen-dawn-70616.herokuapp.com/products/getNumPages'
 
 const ulEL = document.getElementById('sidebarUL')
 const galleryEL = document.getElementById('galleryEL')
+const paginationEL = document.getElementById('paginationEL')
+const breadcrumbEL = document.getElementById('breadcrumbEL')
 
 async function loadData(URL, URL_PROXY) {
-    const fullURL=URL_PROXY+URL
+    const fullURL = URL_PROXY + URL
     return fetch(fullURL, {
         method: 'GET',
     })
@@ -22,11 +26,43 @@ async function loadData(URL, URL_PROXY) {
 async function displayMenu(ul, URL_CATEGORIES, URL_PROXY) {
 
     const responseJson = await loadData(URL_CATEGORIES, URL_PROXY)
-
+    let {name}  = responseJson.data[0]
+    displayBreadcrumb(toCapital(name))
     for (let { id, name } of responseJson.data) {
         const categoryHTML = buildHTMLCategory(id, name)
         ul.appendChild(categoryHTML)
     }
+}
+
+async function displayItems(gallery, URL_PRODUCTS, URL_PROXY, categoryId, page = 1, name) {
+    gallery.innerHTML = ""
+    let filter = `?page=${page ? page : 1}&`
+    filter += name ? `name=${name}` : `categoryId=${categoryId}`
+
+    const responseJson = await loadData(URL_PRODUCTS.concat(filter), URL_PROXY)
+
+    for (let { id, name, url_image, price, discount } of responseJson.data) {
+        const itemHTML = buildHTMLItem(id, name, url_image, price, discount)
+        gallery.appendChild(itemHTML)
+    }
+}
+
+async function displayPagination(pagination, URL_PRODUCTS_GETNUMPAGES, URL_PROXY, categoryId, name) {
+    pagination.innerHTML = ""
+    let filter = `?`
+    filter += name ? `name=${name}` : `categoryId=${categoryId}`
+
+    const responseJson = await loadData(URL_PRODUCTS_GETNUMPAGES.concat(filter), URL_PROXY)
+
+    let {pages}  = responseJson.data[0]
+    for (let step = 0; step < pages; step++) {
+        const paginationHTML = buildHTMLPagination(step+1)
+        pagination.appendChild(paginationHTML)
+    }
+}
+
+function displayBreadcrumb(text){
+    breadcrumbEL.innerHTML=text
 }
 
 async function menuListener() {
@@ -35,7 +71,8 @@ async function menuListener() {
             const link = event.target.href;
             const categoryId = getTextAfterHash(link)
             await displayItems(galleryEL, URL_PRODUCTS, URL_PROXY, categoryId)
-
+            await displayPagination(paginationEL, URL_PRODUCTS_GETNUMPAGES, URL_PROXY, categoryId)
+            displayBreadcrumb(item.innerHTML)
             event.preventDefault()
         });
     });
@@ -53,22 +90,11 @@ function searchListener() {
 
 }
 
-async function displayItems(gallery, URL_PRODUCTS, URL_PROXY, categoryId, page = 1, name) {
-    gallery.innerHTML = ""
-    let filter = `?page=${page ? page:1}&`
-    filter += name ? `name=${name}` : `categoryId=${categoryId}`
-
-    const responseJson = await loadData(URL_PRODUCTS.concat(filter), URL_PROXY)
-
-    for (let { id, name, url_image, price, discount } of responseJson.data) {
-        const itemHTML = buildHTMLItem(id, name, url_image, price, discount)
-        gallery.appendChild(itemHTML)
-    }
-}
 
 async function run() {
     await displayMenu(ulEL, URL_CATEGORIES, URL_PROXY)
     await displayItems(galleryEL, URL_PRODUCTS, URL_PROXY, 1)
+    await displayPagination(paginationEL, URL_PRODUCTS_GETNUMPAGES, URL_PROXY, 1)
     menuListener();
     searchListener();
 }
