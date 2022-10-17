da92c117ec961c571708b9031e7328e5a5aaa21b
Hecey <hecey@hotmail.com>
2022-10-17 02:56:32 -0500

    Changed index html organization

--- js/html/category.js
+++ js/html/category.js
@@ -2,7 +2,7 @@ import { toCapital } from "../helper.js";
 export const buildHTMLCategory = (id, name) => {
     return `
     <li class="nav-item w-100">
-        <a href="#${id}" class="nav-link px-sm-0  pl-sm-3 px-2">
+        <a href="#${id}" class="nav-link px-sm-0  pl-sm-3">
         <i class="fs-5 bi bi-${id}-square"></i><span class="ms-1 d-none d-sm-inline">
         ${toCapital(name)}</span>
         </a>
