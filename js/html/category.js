fac8b913fdfda0ab87b17ad633ba30f327a48128
Hecey <hecey@hotmail.com>
2022-10-17 11:13:58 -0500

    Reefactor index html to remove innecesary tags anda classes

--- js/html/category.js
+++ js/html/category.js
@@ -1,8 +1,8 @@
 import { toCapital } from "../helper.js";
 export const buildHTMLCategory = (id, name) => {
     return `
-    <li class="nav-item w-100">
-        <a href="#${id}" class="nav-link px-sm-0  pl-sm-3">
+    <li class="nav-item w-100 ">
+        <a href="#${id}" class="nav-link   pl-sm-3">
         <i class="fs-5 bi bi-${id}-square"></i><span class="ms-1 d-none d-sm-inline">
         ${toCapital(name)}</span>
         </a>
--- js/html/item.js
+++ js/html/item.js
@@ -1,6 +1,6 @@
 export const buildHTMLItem = (id, name, url_image, price, discount) => {
-    return `<div class="col" ><div class="card shadow-sm" >
-    <div class="border d-flex aligns-items-center justify-content-center">
+    return `<div class="col" ><div class="card shadow-sm m-1" >
+    <div class="border d-flex aligns-items-center justify-content-center ">
     <img class="bd-placeholder-img  py-2 px-2 " src="${!url_image == '' ? url_image : "/images/image-not-found-icon.svg"}" height="200px">
     </div>
     <div class="card-body bg-secondary text-white"><p class="card-text">${name}</p>
