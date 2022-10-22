7eaf88cf1c153a6d59ae0d257df74d90175493c4
Hecey <hecey@hotmail.com>
2022-10-22 08:36:11 -0500

    Fixed svg for image not found using proper html tag

--- js/html/item.js
+++ js/html/item.js
@@ -8,15 +8,21 @@
  * @returns {html}
  */
 export const buildHTMLItem = (id, name, url_image, price, discount) => {
+    const image = (!url_image == '')
+        ? `<img class="img-fluid img-thumbnail"
+                style="border:0;height: auto;max-width: 100%;max-height: 180px;"
+                src="${url_image}">`
+        : `<svg class="" style="">
+                <image xlink:href="/images/image-not-found-icon.svg"/ height="90" x="35%" y="20%" >
+           </svg>`
+
     return `
     <div class="d-flex flex-fill">
         <div class="d-flex flex-fill card shadow-sm m-1">
             <div class="p-2 d-flex aligns-items-center justify-content-center "  style="heigh:280px;">
             <div class="d-flex  flex-column aligns-items-center justify-content-center p-0 m-0 h-100" style="height:100%;min-height: 180px;">
-                <img class="img-fluid img-thumbnail"
-                        style="border:0;height: auto;max-width: 100%;max-height: 180px;"
-                        src="${!url_image == '' ? url_image : "/images/image-not-found-icon.svg"}" >
-                </div>
+                ${image}
+            </div>
             </div>
             <div class="row p-2 m-2 aligns-items-center  justify-content-center card-body bg-secondary text-white "  >
                 <div class="d-flex  flex-column  p-0 m-0 h-100" style="height:100%">
