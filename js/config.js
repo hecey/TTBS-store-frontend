cd268887b712c68eb24b81df20f934b031e5213a
Hecey <hecey@hotmail.com>
2022-11-19 18:55:53 -0500

    Change URL_HOST to render

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 //Configuración de rutas a los diferentes servicios del backend
-export const URL_PROXY = 'https://young-meadow-11122.herokuapp.com/'
-const URL_HOST = 'https://frozen-dawn-70616.herokuapp.com'
+export const URL_PROXY = ''
+const URL_HOST = 'https://bs-store-backend.onrender.com'
 export const URL_CATEGORIES = `${URL_HOST}/categories`
 export const URL_PRODUCTS = `${URL_HOST}/products`
 export const URL_PRODUCTS_GETNUMPAGES = `${URL_HOST}/products/getNumPages`
